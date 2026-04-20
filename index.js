0beb594f1f15b341beabd14d43d42d053ee47b4a
mradevelopermra <121193940+mradevelopermra@users.noreply.github.com>
2026-04-19 19:25:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -140,8 +140,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("🚀 Servidor WebSocket corriendo en puerto", PORT);
+server.listen(3000, () => {
+  console.log("🚀 Servidor WebSocket corriendo en puerto 3000");
 });
